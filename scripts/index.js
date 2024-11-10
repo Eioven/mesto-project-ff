4b990bd19eabc83a031bd770445ba93da65a0828
Zirael <iicristal@mail.ru>
2024-11-10 16:08:17 +0300

    first--version

--- scripts/index.js
+++ scripts/index.js
@@ -1,17 +1,16 @@
 // @todo: Темплейт карточки
 const cardTemplate = document.querySelector("#card-template").content;
 // @todo: DOM узлы
-const cardsContainer = document.querySelector(".places__list")
+const cardsContainer = document.querySelector(".places__list");
 // @todo: Функция создания карточки
 function createCard (cardContent, deleteCard) {
     const card = cardTemplate.querySelector(".card").cloneNode(true);
     const cardImage = card.querySelector(".card__image");
     const cardTitle = card.querySelector(".card__title");
-    const deleteButton = card.querySelector(".card__delete-button")
+    const deleteButton = card.querySelector(".card__delete-button");
 
     cardImage.src = cardContent.link;
     cardImage.alt = cardContent.name;
-
 
     cardTitle.textContent = cardContent.name;
 
@@ -21,9 +20,9 @@ function createCard (cardContent, deleteCard) {
 }
 // @todo: Функция удаления карточки
 function deleteCard(event) {
-    event.target.closest(".places__item").remove()
+    event.target.closest(".card").remove();
 }
-// @todo: Вывести карточки на страницу
+// @todo: Вывеcти карточки на страницу
 function addCards (array){
     array.forEach(function (cardContent){
         const newCard = createCard(cardContent, deleteCard);
